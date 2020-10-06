bf4b0a6da8eb333b545ee6299f3e88b7ef732415
frankylee kelly <frankyleekelly@gmail.com>
2020-10-06 12:52:23 -0700

    Added Donate and Rescue controllers; added Views and subpages

---
+++ Controllers/RescueController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SarasTreasures.Controllers
+{
+    public class RescueController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adopt()
+        {
+            return View();
+        }
+
+        public IActionResult HappyTails()
+        {
+            return View();
+        }
+
+        public IActionResult Resources()
+        {
+            return View();
+        }
+
+    }
+}
